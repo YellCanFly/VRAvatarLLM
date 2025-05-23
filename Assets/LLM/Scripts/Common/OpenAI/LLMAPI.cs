6f536f03dd39469fdc3fe48a3fc96e6113686602
YCanFly <501917040@qq.com>
2025-05-23 16:33:53 +0200

    # Re-code the LLMAPI, including: 1. LLMAPI as base class, got basic functions (STT, LLM, TTS) 2. Add child class example (Virtual Agent, define input and output structure, fill input data, parse and use output data) 3.. Finished change LLMAPI to Chat API (Left todo: maximun memory)

--- Assets/LLM/Scripts/Common/OpenAI/LLMAPI.cs
+++ Assets/LLM/Scripts/Common/OpenAI/LLMAPI.cs
@@ -1,27 +1,24 @@
-using OpenAI;
-using OpenAI.Realtime;
-using OpenAI.Models;
-using OpenAI.Audio;
-
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Threading.Tasks;
-using System.Collections;
-using System.Threading;
-using System.Linq;
-
 using UnityEngine;
-using UnityEngine.Events;
+using TMPro;
 
 using Utilities.Audio;
 using Utilities.Encoding.Wav;
 
-using TMPro;
-using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+using OpenAI;
+using OpenAI.Models;
 using OpenAI.Chat;
-using Unity.XR.CoreUtils.Collections;
-using UnityEngine.UI;
+using OpenAI.Audio;
+using System.Collections;
+using UnityEngine.Events;
+using UnityEditor.Experimental.GraphView;
+
+
 
 [RequireComponent(typeof(AudioSource))]
 public class LLMAPI : MonoBehaviour
@@ -29,10 +26,10 @@ public class LLMAPI : MonoBehaviour
     // OpenAI Variables
     [Header("OpenAI Settings")]
     public bool enableDebug = false;
-    private OpenAIClient openAI;
-    private Model llmModel = new Model("o4-mini", "openai");
-    private List<Message> messages = new List<Message>();
-    private JsonSchema outputSchema;
+    protected OpenAIClient openAI;
+    protected Model llmModel = new Model("o4-mini", "openai");
+    protected Model ttsModel = new Model("tts-1", "openai");
+    protected List<Message> messages = new List<Message>();
 
     // System prompt for the assistant
     [Header("Prompt Settings")]
@@ -42,13 +39,18 @@ public class LLMAPI : MonoBehaviour
     [Header("Interaction Settings")]
     public GazeSphereDetector gazeSphereDetector;
     public AvatarController avatarController;
+    protected string currentPointingObject = "";
 
-    // UI references
-    public TMP_InputField inputField;
-    public TextMeshProUGUI responseText;
+    [Header("Voice Recording Settings")]
+    public bool isRecording;
+    private AudioSource audioSource;
+    private MemoryStream transcriptionStream;
+    private CancellationTokenSource recordingCts;
+    protected UnityAction<float> onStartSpeak;
+    protected UnityAction onFinishSpeak;
 
     // Debug Vars
-    private float debugTime;
+    protected float debugTime;
 
     private void Awake()
     {
@@ -56,15 +58,10 @@ public class LLMAPI : MonoBehaviour
         Init();
     }
 
-    private void Start()
-    {
-
-    }
-
     /// <summary>
     /// Initialize OpenAI real-time session, configure authentication, start receiving events.
     /// </summary>
-    public void Init()
+    protected virtual void Init()
     {
         // Load authentication from file
         string authPath;
@@ -83,6 +80,34 @@ public class LLMAPI : MonoBehaviour
         RecordingManager.EnableDebug = enableDebug;
         ResetMessages();
 
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    /// <summary>
+    /// Update function to check for user input (VR or Keyboard) to start/stop recording.
+    /// </summary>
+    protected virtual void Update()
+    {
+        bool isVRConnected = OVRInput.IsControllerConnected(OVRInput.Controller.LTouch);
+
+        if (
+            (isVRConnected && OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.LTouch)) ||
+            (!isVRConnected && Input.GetKeyDown(KeyCode.Space))
+        )
+        {
+            Debug.Log("Start recording triggered");
+            StartMicRecording();
+        }
+
+        // ËÉ¿ªÍ£Ö¹Â¼Òô
+        if (
+            (isVRConnected && OVRInput.GetUp(OVRInput.Button.One, OVRInput.Controller.LTouch)) ||
+            (!isVRConnected && Input.GetKeyUp(KeyCode.Space))
+        )
+        {
+            Debug.Log("Stop recording triggered");
+            StopMicRecording();
+        }
     }
 
     public void ResetMessages()
@@ -91,90 +116,13 @@ public class LLMAPI : MonoBehaviour
         messages.Add(new Message(Role.System, systemPrompt));
     }
 
-    public async void UserChatInput(string userContent)
+    /// <summary>
+    /// Virtual function to handle user's input (Implemented in child classses)
+    /// </summary>
+    /// <param name="userContent"></param>
+    virtual public async void UserChatInput(string userContent)
     {
-        // Init user input object
-        UserInput userInput = new();
-        userInput.question = userContent;
-
-
-        // Parse gaze data from GazeSphereDetector
-        if (gazeSphereDetector != null)
-        {
-            string gazeObjectName = gazeSphereDetector.GetLatestGazeObject(); // string For the latest gaze object
-            var gazeObjectNameList = gazeSphereDetector.GetGazeObjectList(); // List<string> For gaze history
-            var allObjectInEyeFieldList = gazeSphereDetector.GetAllObjectInEyeFieldList(); // List<string> For all objects in eye field
-
-            Dictionary<string, RelativePosition> objRelativePosDict = new();
-            foreach (var obj in allObjectInEyeFieldList)
-            {
-                Debug.Log($"Object in eye field: {obj}");
-                var interactObj = InteractObjectManager.Instance?.GetObjectByName(obj);
-                if (interactObj != null && !objRelativePosDict.ContainsKey(obj))
-                {
-                    objRelativePosDict.Add(obj, interactObj.GetRelativePositionToMainCameraDirection());
-                }
-            }
-            foreach (var obj in gazeObjectNameList)
-            {
-                var interactObj = InteractObjectManager.Instance?.GetObjectByName(obj);
-                if (interactObj != null && !objRelativePosDict.ContainsKey(obj))
-                {
-                    objRelativePosDict.Add(obj, interactObj.GetRelativePositionToMainCameraDirection());
-                }
-            }
-
-            userInput.current_gaze_object = gazeObjectName;
-            userInput.gaze_history = gazeObjectNameList;
-            userInput.objects_in_view = allObjectInEyeFieldList;
-            userInput.objects_info = objRelativePosDict.Select(kvp => new ObjectInfo
-            {
-                object_name = kvp.Key,
-                object_relative_position = kvp.Value
-            }).ToList();
-        }
-        else
-        {
-            // Fallback to default values if GazeSphereDetector is not available
-            userInput.current_gaze_object = "null";
-            userInput.gaze_history = new List<string>();
-            userInput.objects_in_view = new List<string>();
-            userInput.objects_info = new List<ObjectInfo>();
-        }
-
-        // Add user input to the message list
-        string userInputJson = JsonUtility.ToJson(userInput, true);
-        messages.Add(new Message(Role.User, userInputJson));
-        Debug.Log($"User: {userInputJson}");
-
-        // Create a chat request and send it to OpenAI
-        var chatRequest = new ChatRequest(messages, llmModel);
-        var (jsonObjResponse, response) = await openAI.ChatEndpoint.GetCompletionAsync<AIResponse>(chatRequest);
-        //var response = await openAI.ChatEndpoint.GetCompletionAsync(chatRequest);
-
-
-        responseText.text = response;
-        // If the response is not null, update the UI and log the response
-        if (jsonObjResponse != null)
-        {
-            messages.Add(new Message(Role.Assistant, response)); // Add assistant response to the message list
-            responseText.text = jsonObjResponse.answer;
-            Debug.Log($" AI: {jsonObjResponse.answer} | Pointing: {jsonObjResponse.gaze_and_pointing_object}");
-        }
-        else
-        {
-            Debug.LogError("Response is null!");
-        }
-    }
-
-    public void OnSendButtonClick()
-    {
-        string userInput = inputField.text;
-        if (!string.IsNullOrEmpty(userInput))
-        {
-            UserChatInput(userInput);
-            inputField.text = string.Empty;
-        }
+        Debug.Log("User input: " + userContent);
     }
 
     /// <summary>
@@ -188,111 +136,125 @@ public class LLMAPI : MonoBehaviour
             Debug.LogWarning("systemPromptAsset is not valid!");
     }
 
-    [ContextMenu("Test")]
-    public async void Test()
+    /// <summary>
+    /// Convert text response to speech and play it.
+    /// </summary>
+    protected async Task TextToSpeechRequest(string speechContent)
     {
-        messages = new List<Message>
+        debugTime = Time.time;
+        var speechRequest = new SpeechRequest(
+            model: ttsModel,
+            input: speechContent,
+            responseFormat: SpeechResponseFormat.PCM);
+
+        var speechClip = await openAI.AudioEndpoint.GetSpeechAsync(speechRequest);
+        audioSource.clip = speechClip;
+        audioSource.Play();
+        Debug.Log("Clip Length = " + speechClip.Length);
+        AvatarStartPointingByName(currentPointingObject, speechClip.Length);
+
+        // Debug output
+        float ttsTime = Time.time - debugTime;
+        debugTime = Time.time;
+        Debug.Log(speechClip);
+        Debug.Log("TTS Time = " + ttsTime);
+    }
+
+    #region Microphone Record
+    /// <summary>
+    /// Start microphone recording and stream to memory.
+    /// </summary>
+    protected void StartMicRecording()
+    {
+        Debug.Log("Recording started...");
+        debugTime = Time.time;
+        isRecording = true;
+        recordingCts = new CancellationTokenSource();
+        transcriptionStream = new MemoryStream();
+
+        RecordingManager.StartRecordingStream<WavEncoder>(async (buffer) =>
         {
-            new Message(Role.System, "You are a helpful assistant."),
-            new Message(Role.User, "Who won the world series in 2020?"),
-            new Message(Role.Assistant, "The Los Angeles Dodgers won the World Series in 2020."),
-            new Message(Role.User, "Where was it played?"),
-        };
-
-        Debug.Log("Test");
-        var chatRequest = new ChatRequest(messages, llmModel);
-        var response = await openAI.ChatEndpoint.GetCompletionAsync(chatRequest);
-        Debug.Log("Choice Count = " + response.Choices.Count);
-        var choice = response.FirstChoice;
-        Debug.Log($"[{choice.Index}] {choice.Message.Role}: {choice.Message} | Finish Reason: {choice.FinishReason}");
+            if (!recordingCts.Token.IsCancellationRequested)
+            {
+                await transcriptionStream.WriteAsync(buffer, CancellationToken.None);
+            }
+        }, 24000, recordingCts.Token);
     }
-
-    [ContextMenu("Test2")]
-    public async void Test2()
-    {
-        messages = new List<Message>
-        {
-            new(Role.System, "You are a helpful math tutor. Guide the user through the solution step by step."),
-            new(Role.User, "how can I solve 8x + 7 = -23")
-        };
-
-        //var chatRequest = new ChatRequest(messages, model: "gpt-4o-2024-08-06", jsonSchema: outputSchema);
-        //var response = await openAI.ChatEndpoint.GetCompletionAsync(chatRequest);
-        //Debug.Log("Response: " + response);
-
-        var chatRequest = new ChatRequest(messages, model: "gpt-4o-2024-08-06");
-        var (mathResponse, chatResponse) = await openAI.ChatEndpoint.GetCompletionAsync<MathResponse>(chatRequest);
-
-        for (var i = 0; i < mathResponse.Steps.Count; i++)
-        {
-            var step = mathResponse.Steps[i];
-            Debug.Log($"Step {i}: {step.Explanation}");
-            Debug.Log($"Result: {step.Output}");
-        }
-
-        Debug.Log($"Final Answer: {mathResponse.FinalAnswer}");
-        chatResponse.GetUsage();
-    }
-
-
-    [System.Serializable]
-    public class UserInput
-    {
-        [JsonProperty("question")]
-        public string question;
-
-        [JsonProperty("current_gaze_object")]
-        public string current_gaze_object;
-
-        [JsonProperty("gaze_history")]
-        public List<string> gaze_history;
-
-        [JsonProperty("objects_in_view")]
-        public List<string> objects_in_view;
-
-        [JsonProperty("objects_info")]
-        public List<ObjectInfo> objects_info;
-    }
-
-    [System.Serializable]
-    public class ObjectInfo
-    {
-        [JsonProperty("object_name")]
-        public string object_name;
-
-        [JsonProperty("object_relative_position")]
-        public RelativePosition object_relative_position;
-    }
-
 
     /// <summary>
-    /// Serializable response class for JSON parsing.
+    /// Stop recording and send audio for transcription.
     /// </summary>
-    [System.Serializable]
-    public class AIResponse
+    protected async void StopMicRecording()
     {
-        [JsonProperty("answer")]
-        public string answer { get; private set; }
+        float speakingTime = Time.time - debugTime;
+        debugTime = Time.time;
+        Debug.Log("Stopping recording... ");
+        Debug.Log("Speaking Time = " + speakingTime);
 
-        [JsonProperty("gaze_and_pointing_object")]
-        public string gaze_and_pointing_object { get; private set; }
+        isRecording = false;
+        recordingCts?.Cancel();
+
+        transcriptionStream.Position = 0;
+
+        var audioRequest = new AudioTranscriptionRequest(
+            audio: transcriptionStream,
+            audioName: "mic_audio.wav",
+            model: Model.Whisper1,
+            language: "en"
+        );
+
+        try
+        {
+            var result = await openAI.AudioEndpoint.CreateTranscriptionTextAsync(audioRequest);
+            float transcriptionTime = Time.time - debugTime;
+            debugTime = Time.time;
+            Debug.Log("Transcription result: " + result);
+            Debug.Log("Transcription Time = " + transcriptionTime);
+            UserChatInput(result);
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("Transcription failed: " + e);
+        }
+        finally
+        {
+            await transcriptionStream.DisposeAsync();
+        }
+    }
+    #endregion
+
+
+    #region Avatar Animation
+    protected void AvatarStartPointingByName(string objectName, float pointDuration)
+    {
+        var gazeObject = InteractObjectManager.Instance?.GetObjectByName(objectName);
+        if (gazeObject != null)
+        {
+            Vector3 gazeObjectPosition = gazeObject.transform.position;
+            // Todo: Add gaze and point logic here
+            if (avatarController != null)
+            {
+                avatarController.LookAtTarget.transform.position = gazeObjectPosition;
+                avatarController.StartPointing();
+                StartCoroutine(AvatarKeepPointingInDuration(pointDuration));
+            }
+        }
+        else
+        {
+            Debug.LogWarning($"Gaze object '{objectName}' not found.");
+        }
     }
 
-    public class MathResponse
+    /// <summary>
+    /// Keep pointing for a duration then stop pointing (Back to normal gesture)
+    /// </summary>
+    /// <param name="pointDuration"></param>
+    /// <returns></returns>
+    IEnumerator AvatarKeepPointingInDuration(float pointDuration)
     {
-        [JsonProperty("steps")]
-        public IReadOnlyList<MathStep> Steps { get; private set; }
-
-        [JsonProperty("final_answer")]
-        public string FinalAnswer { get; private set; }
+        yield return new WaitForSeconds(pointDuration);
+        avatarController.StopPointing();
+        currentPointingObject = "";
     }
-
-    public class MathStep
-    {
-        [JsonProperty("explanation")]
-        public string Explanation { get; private set; }
-
-        [JsonProperty("output")]
-        public string Output { get; private set; }
-    }
+    #endregion
 }
---
+++ Assets/LLM/Scripts/Common/OpenAI/LLMAPIVirtualAgent.cs
@@ -0,0 +1,179 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+using OpenAI.Chat;
+using OpenAI;
+
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+
+using Newtonsoft.Json;
+using TMPro;
+
+
+public class LLMAPIVirtualAgent : LLMAPI
+{
+    [Header("UI Settings")]
+    // UI references
+    public TMP_InputField inputField;
+    public TextMeshProUGUI responseText;
+    public Button sendButton;
+
+    protected override void Init()
+    {
+        base.Init();
+        if (sendButton != null )
+        {
+            sendButton.onClick.AddListener(OnSendButtonClick);
+        }
+    }
+
+    override public async void UserChatInput(string userContent)
+    {
+        debugTime = Time.time;
+
+        // Init user input object
+        UserInput userInput = new();
+        userInput.question = userContent;
+
+        // Parse gaze data from GazeSphereDetector
+        if (gazeSphereDetector != null)
+        {
+            string gazeObjectName = gazeSphereDetector.GetLatestGazeObject(); // string For the latest gaze object
+            var gazeObjectNameList = gazeSphereDetector.GetGazeObjectList(); // List<string> For gaze history
+            var allObjectInEyeFieldList = gazeSphereDetector.GetAllObjectInEyeFieldList(); // List<string> For all objects in eye field
+
+            Dictionary<string, RelativePosition> objRelativePosDict = new();
+            foreach (var obj in allObjectInEyeFieldList)
+            {
+                Debug.Log($"Object in eye field: {obj}");
+                var interactObj = InteractObjectManager.Instance?.GetObjectByName(obj);
+                if (interactObj != null && !objRelativePosDict.ContainsKey(obj))
+                {
+                    objRelativePosDict.Add(obj, interactObj.GetRelativePositionToMainCameraDirection());
+                }
+            }
+            foreach (var obj in gazeObjectNameList)
+            {
+                var interactObj = InteractObjectManager.Instance?.GetObjectByName(obj);
+                if (interactObj != null && !objRelativePosDict.ContainsKey(obj))
+                {
+                    objRelativePosDict.Add(obj, interactObj.GetRelativePositionToMainCameraDirection());
+                }
+            }
+
+            userInput.current_gaze_object = gazeObjectName;
+            userInput.gaze_history = gazeObjectNameList;
+            userInput.objects_in_view = allObjectInEyeFieldList;
+            userInput.objects_info = objRelativePosDict.Select(kvp => new ObjectInfo
+            {
+                object_name = kvp.Key,
+                object_relative_position = kvp.Value
+            }).ToList();
+        }
+        else
+        {
+            // Fallback to default values if GazeSphereDetector is not available
+            userInput.current_gaze_object = "null";
+            userInput.gaze_history = new List<string>();
+            userInput.objects_in_view = new List<string>();
+            userInput.objects_info = new List<ObjectInfo>();
+        }
+
+        // Add user input to the message list
+        string userInputJson = JsonUtility.ToJson(userInput, true);
+        messages.Add(new Message(Role.User, userInputJson));
+        Debug.Log($"User: {userInputJson}");
+
+        // Create a chat request and send it to OpenAI, wait until get response
+        var chatRequest = new ChatRequest(messages, llmModel);
+        var (jsonObjResponse, response) = await openAI.ChatEndpoint.GetCompletionAsync<AIResponse>(chatRequest);
+
+        // Debug print the LLM's inference and data communication time
+        float llmTime = Time.time - debugTime;
+        Debug.Log("LLM Time = " + llmTime + " s");
+
+        // Handle response
+        HandleAIResponse(jsonObjResponse, response);
+    }
+
+    public async void HandleAIResponse(AIResponse jsonObjResponse, ChatResponse rawResponse)
+    {
+        if (jsonObjResponse == null)
+        {
+            Debug.LogError("Error: Response is null!");
+            return;
+        }
+
+        Debug.Log("Raw Chat Response: " + rawResponse);
+
+        // Add response to message list
+        messages.Add(new Message(Role.Assistant, rawResponse));
+
+        // Update UI textg
+        responseText.text = jsonObjResponse.answer;
+
+        // Update current point object
+        currentPointingObject = jsonObjResponse.gaze_and_pointing_object;
+
+        // Send TTS Request
+        await TextToSpeechRequest(jsonObjResponse.answer);
+    }
+
+
+    public void OnSendButtonClick()
+    {
+        string userInput = inputField.text;
+        if (!string.IsNullOrEmpty(userInput))
+        {
+            UserChatInput(userInput);
+            inputField.text = string.Empty;
+        }
+    }
+
+    // Structures
+
+    [System.Serializable]
+    public class UserInput
+    {
+        [JsonProperty("question")]
+        public string question;
+
+        [JsonProperty("current_gaze_object")]
+        public string current_gaze_object;
+
+        [JsonProperty("gaze_history")]
+        public List<string> gaze_history;
+
+        [JsonProperty("objects_in_view")]
+        public List<string> objects_in_view;
+
+        [JsonProperty("objects_info")]
+        public List<ObjectInfo> objects_info;
+    }
+
+    [System.Serializable]
+    public class ObjectInfo
+    {
+        [JsonProperty("object_name")]
+        public string object_name;
+
+        [JsonProperty("object_relative_position")]
+        public RelativePosition object_relative_position;
+    }
+
+
+    /// <summary>
+    /// Serializable response class for JSON parsing.
+    /// </summary>
+    [System.Serializable]
+    public class AIResponse
+    {
+        [JsonProperty("answer")]
+        public string answer { get; private set; }
+
+        [JsonProperty("gaze_and_pointing_object")]
+        public string gaze_and_pointing_object { get; private set; }
+    }
+}
