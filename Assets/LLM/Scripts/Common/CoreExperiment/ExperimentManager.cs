9be46f2e0ebf0ba29d993216d5b968058ea8c3b1
YCanFly <501917040@qq.com>
2025-05-30 17:53:39 +0200

    # Add tutorial level and experiment manager 1. add 3d interactive UI (including virtual keyboard etc.) 2. add participants ID input and validate process 3. add latin square to generate the experimetn order sequence 4. modified some project settings to fix bug in task2 merging 5. add necessary package for 3DUI package

---
+++ Assets/LLM/Scripts/Common/CoreExperiment/ExperimentManager.cs
@@ -0,0 +1,67 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ExperimentManager : MonoBehaviour
+{
+    public static int participantCount = 24; // Total number of participants in the experiment
+    public static int participantID = 0; // Participant ID for the experiment
+    public InteractCondition[] conditionOrder;
+
+    public UnityAction onParticipantIDConfirmed;
+
+
+    private static readonly InteractCondition[] Conditions = {
+        InteractCondition.Baseline,
+        InteractCondition.UniDirectional_Input,
+        InteractCondition.UniDirectional_Output,
+        InteractCondition.BiDirectional
+    };
+
+    private static readonly int[][] latingSquare = {
+        new int[] { 1, 2, 3, 4 },
+        new int[] { 2, 1, 4, 3 },
+        new int[] { 3, 4, 1, 2 },
+        new int[] { 4, 3, 1, 1 }
+    };
+
+    public static List<InteractCondition> GetConditionOrder(int participantID)
+    {
+        if (participantID < 1 || participantID > participantCount)
+        {
+            Debug.LogError("Participant ID must be between 1 and " + participantCount);
+            return null;
+        }
+
+        int baseIndex = (participantID - 1) % Conditions.Length;
+        List<InteractCondition> order = new();
+
+        for (int i = 0; i < Conditions.Length; i++)
+        {
+            int idx = latingSquare[baseIndex][i] - 1;
+            order.Add(Conditions[idx]);
+        }
+
+        return order;
+    }
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        onParticipantIDConfirmed += OnParticipantIDConfirmed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnParticipantIDConfirmed()
+    {
+        GetConditionOrder(participantID);
+    }
+
+
+}
