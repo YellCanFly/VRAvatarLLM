89aca6b70ab4f2c0cd3b3596177c39c528c82156
YCanFly <501917040@qq.com>
2025-06-02 23:00:57 +0200

    # add new gather item object class

--- Assets/LLM/Scripts/1_GatherItemGame/GatherItemObject.cs
+++ Assets/LLM/Scripts/1_GatherItemGame/GatherItemObject.cs
@@ -1,3 +1,4 @@
+using Newtonsoft.Json;
 using UnityEngine;
 
 [RequireComponent(typeof(InteractObject))]
@@ -25,4 +26,25 @@ public class GatherItemObject : MonoBehaviour
             renderer.gameObject.layer = LayerMask.NameToLayer("Default"); // Set the layer back to default
         }
     }
+}
+
+[System.Serializable]
+public class GatherItemObjectInfo
+{
+    
+    [JsonProperty("object_name")]
+    public string object_name;
+
+    [JsonProperty("object_description")]
+    public string object_description;
+
+    [JsonProperty("object_relative_position_to_user")]
+    public RelativePosition object_relative_position_to_uesr;
+
+    [JsonProperty("object_relative_position_to_avatar")]
+    public RelativePosition object_relative_position_to_avatar;
+
+    [JsonProperty("object_size")]
+    public Vector3 object_size;
+
 }
